59389af37fd7659e34d5a481b4f2d57e3a33c714
PranjalAlbhar <34841836+PranjalAlbhar@users.noreply.github.com>
2019-04-16 15:38:56 +0530

    Add files via upload

---
+++ src_assignement13/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignemt';
+
+   fun()
+  {
+    this.title='Marvellous';
+    return this.title;
+
+  }
+}
