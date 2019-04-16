0e9ff770f69317cc24b85fa47afb12321bae4a7a
PranjalAlbhar <34841836+PranjalAlbhar@users.noreply.github.com>
2019-04-16 13:15:13 +0530

    Add files via upload

---
+++ src_assignment6/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//Create application which will display one textfield and one static text “Marvellous
+//Infosystems” on page.
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Marvellous Infosystems';
+}
